c0fe99fdf8d8afc229098fe8ed4e0cc6b2c4c4bb
GeorgeMosch <georgemoschovitis@hotmail.co.uk>
2020-11-11 09:59:29 +0200

    Created the backend and partially the front end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
+function App() {
+  return (
+    <div>
+      <h1>Hi World!</h1>
+    </div>
+  );
+}
+
+export default App;
